12c423b094e99427ce08ba20157472266d75644f
RAJWARDHAN-B <rajbravo5613@gmail.com>
2025-02-18 17:30:58 +0530

    added footer and navbar to some pages

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./HomePage.css";
 import Sustainability from "./Sustainability";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const AboutUs = () => {
   return (
     <div>
       <Navbar/>
       <Sustainability/>
+      <Footer/>
     </div>
   );
 };
